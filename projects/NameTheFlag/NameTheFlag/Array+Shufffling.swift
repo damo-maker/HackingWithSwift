621ef3993182aaba81d89285d4e5d09ea4a933c7
damo-maker <damianjohns89@gmail.com>
2020-09-22 00:51:39 +1000

    add NameTheFlag project2

---
+++ projects/NameTheFlag/NameTheFlag/Array+Shufffling.swift
@@ -0,0 +1,36 @@
+// (c) 2015 Nate Cook, licensed under the MIT license
+//
+// Fisher-Yates shuffle as protocol extensions
+
+import UIKit
+
+extension Collection {
+    /// Return a copy of `self` with its elements shuffled
+    func shuffled() -> [Iterator.Element] {
+        let list = Array(self)
+        _ = list.shuffled()
+        return list
+    }
+}
+
+extension MutableCollection where Index == Int {
+    /// Shuffle the elements of `self` in-place.
+    mutating func shuffleInPlace() {
+        // empty and single-element collections don't shuffle
+        if count < 2 { return }
+        
+        for i in startIndex..<(endIndex - 1) {
+            let j = Int(arc4random_uniform(UInt32(endIndex - i))) + i
+            guard i != j else { continue }
+            swap(&self[i], &self[j])
+        }
+    }
+}
+
+//[1, 2, 3].shuffle()
+// [2, 3, 1]
+//let fiveStrings = (0.stride(to: 100, by: 5).map(String.init)).shuffle()
+// ["20", "45", "70", "30", ...]
+//var numbers = [1, 2, 3, 4]
+//numbers.shuffleInPlace()
+// [3, 2, 1, 4]
---
+++ projects/NameTheFlag/NameTheFlag/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  NameTheFlag
+//
+//  Created by Damian Johns on 4/06/2020.
+//  Copyright © 2020 Damian Johns. All rights reserved.
+//
+
+import GameplayKit
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func askQuestion(_ action: UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        var messageTitle: String
+        
+        if sender.tag == correctAnswer {
+            messageTitle = "Correct"
+            score += 1
+        } else {
+            messageTitle = "Wrong"
+            score -= 1
+        }
+        
+        let messageText = "Your score is \(score)"
+        
+        let ac = UIAlertController(title: messageTitle, message: messageText, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true, completion: nil)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
