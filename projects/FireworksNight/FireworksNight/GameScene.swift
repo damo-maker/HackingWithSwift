6e18ec8b9e24632bdfdddde8d1af7a121794c22b
damo-maker <damianjohns89@gmail.com>
2020-10-18 18:43:49 +1100

    update ViewController.swift

--- projects/FireworksNight/FireworksNight/GameScene.swift
+++ projects/FireworksNight/FireworksNight/GameScene.swift
@@ -163,8 +163,9 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        checkTouches(touches)
-    }
+        checkTouches(touches) 
+
+                    }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
